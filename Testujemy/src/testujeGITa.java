28af3d50b0c68d7252e133ce5c88b8b0ea8cc4fc
robertkkrakow <robertk.krakow@gmail.com>
2017-03-08 22:41:44 +0100

    test 1

---
+++ Testujemy/src/testujeGITa.java
@@ -0,0 +1,12 @@
+
+public class testujeGITa {
+
+	public static void main(String args[]){
+		
+		int a = 4, b = 6;
+		
+		System.out.println("a = " + a + " b = " + b + " Wynik dodawania: " + (a+b));
+		
+	}
+	
+}
